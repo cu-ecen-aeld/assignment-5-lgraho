4eb4cec5e7b1830f806dfb3a98d94b688c5b44b0
lgraho <84180388+lgraho@users.noreply.github.com>
2026-01-20 16:54:21 +0100

    add ldd package and rootfs overlay containing related init script (assignment 7 part 2)

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,19 @@
+
+##############################################################
+#
+# Linux Device Drivers examples
+#
+##############################################################
+
+LDD_VERSION = 4a86e74435671b037613e409984cfa3d7518af82
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-lgraho.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules scull
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
